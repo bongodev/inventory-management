33c55ff36d6bb67faf0447672029f4773407c8dc
kmtusher97 <kmtusher97@gmail.com>
2024-02-20 22:03:10 +0600

    Add ui dir to export MUI component
    Customize Box component

---
+++ inventory-client/src/ui/index.jsx
@@ -0,0 +1,28 @@
+import {
+  AppBar,
+  Avatar,
+  Button,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from '@mui/material';
+
+import { Box } from './Box';
+
+export {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip,
+  Typography,
+};
