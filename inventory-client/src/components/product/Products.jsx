b28482edb06f759395b2b6321c0cbf23721f7e66
kmtusher97 <kmtusher97@gmail.com>
2024-02-27 20:54:32 +0600

    How use effect works

--- inventory-client/src/components/product/Products.jsx
+++ inventory-client/src/components/product/Products.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { useProducts } from '../../hooks';
 
 import { ProductCard } from './productCard/ProductCard';
@@ -6,9 +7,27 @@ import './Products.css';
 
 export const Products = () => {
   const { products } = useProducts();
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => setCount(count + 1);
+
+  useEffect(() => {
+    if (count > 4) {
+      alert('Reached maximum');
+    }
+    console.log('inside useEffect 1');
+  }, [count]);
+
+  useEffect(() => {
+    console.log('inside useEffect 2');
+  }, []);
+
+  console.log('rendering....');
 
   return (
     <div className="products">
+      <button onClick={increaseCount}>+</button>
+      <p>{count}</p>
       <div className="products-grid">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
