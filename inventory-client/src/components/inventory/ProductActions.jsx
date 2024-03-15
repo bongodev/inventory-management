01cfe29a9885f2b55027bea3cd333c4d83abd7f2
kmtusher97 <kmtusher97@gmail.com>
2024-03-15 16:28:31 +0600

    add reload page after deleting

--- inventory-client/src/components/inventory/ProductActions.jsx
+++ inventory-client/src/components/inventory/ProductActions.jsx
@@ -21,7 +21,7 @@ export const ProductActions = ({ productRowData }) => {
   const onConfirmProductDelete = () => {
     setIsDeleting(true);
     ProductServices.deleteProduct(productToDelete)
-      .then()
+      .then(() => window.location.reload())
       .catch((err) => {
         console.error(err);
         alert('Failed to delete');
