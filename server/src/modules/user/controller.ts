9c8ad010648d98ffda564114567595068cf39f32
kmtusher97 <kmtusher97@gmail.com>
2026-01-10 23:13:11 +0600

    server: add user service

--- server/src/modules/user/controller.ts
+++ server/src/modules/user/controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
+import { userService } from ".";
 
-export const createUser = (req: Request, res: Response) => {
-  // Logic to create a user
-  console.log("User created:", req.body);
-  res.status(201).send("User created");
+export const createUser = async (req: Request, res: Response) => {
+  const newUser = await userService.createUser(req.body);
+  res.status(201).json(newUser);
 };
