524f3f1124e218e5410f074ad49fe75c37235856
kmtusher97 <kmtusher97@gmail.com>
2026-01-18 22:32:06 +0600

    server: add authorization middleware

--- server/src/middlewares/auth.ts
+++ server/src/middlewares/auth.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 
+import { UserRole } from '@/types';
+
 export const authenticate = (
   req: Request,
   res: Response,
@@ -9,4 +11,13 @@ export const authenticate = (
     return next();
   }
   return res.status(401).json({ message: 'Unauthorized' });
+};
+
+export const authorize = (roles: UserRole[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (req.user && roles.includes(req.user.role)) {
+      return next();
+    }
+    return res.status(403).json({ message: 'Forbidden' });
+  };
 };
